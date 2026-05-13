efd200d0a80f52627832ea858dd84c6c592350db
jmneoda <j.neoda.533687@umindanao.edu.ph>
2026-05-13 19:48:20 +0800

    update commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,31 +1,53 @@
 <?php
 
+
+
 namespace App\Providers;
 
+
+
 use Illuminate\Support\ServiceProvider;
+
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Artisan;
+
+
 
 class AppServiceProvider extends ServiceProvider
+
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
 
     /**
-     * Bootstrap any application services.
+
+     * Register any application services.
+
      */
-    public function boot(): void
+
+    public function register(): void
+
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-            Artisan::call('view:clear');
-            Artisan::call('cache:clear');
-            Artisan::call('config:clear');
-        }
+
+        //
+
     }
+
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
+
+    public function boot(): void
+
+    {
+
+        if (env('APP_ENV') === 'production') {
+
+            URL::forceScheme('https');
+
+        }
+
+    }
+
 }
